9fd85393f988e00c480cbd2b0d1defb6d5f19fb6
Vova <example@gmail.com>
2022-10-18 11:56:24 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+/* const answers = [];
+
+answers[0] = prompt('Whats your name?', '');
+answers[1] = prompt('Whats your subname?', '');
+answers[2] = prompt('Whats your age?', '');
+
+console.log(typeof (answers));
+console.log(typeof (null));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`); */
+
+/* const user = "Ivan";
+
+alert(`Привет, ${user}`) */
+
+console.log('arr' + "- object");
+console.log(4 + "5");
+
+let incr = 10,
+	 decr = 10;
+
+/* ++incr;
+--decr; */
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 != '6');
+
+const isChecked = false,
+		isClose = false;
+
+console.log(isChecked || isClose);
